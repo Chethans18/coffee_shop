df0aa608d6c44ea37d073a68ef0d066d14129237
Chethan S <chethans17jan2004@gmail.com>
2024-02-06 06:10:47 +0530

    media queries

---
+++ scriptfiles/script.js
@@ -0,0 +1,6 @@
+document.addEventListener("DOMContentLoaded", function () {
+    var shopNow = document.getElementById("shopNow");
+    shopNow.addEventListener("click", function () {
+        window.location.href = "../cssfiles/shop.html"
+    });
+})
